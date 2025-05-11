f7a7e86b884b760473611fb61b66b8d86317d6d0
Yugo <pikkun0907@gmail.com>
2025-05-11 12:07:10 +0200

    init

--- RL_PPO/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-
-    score_history = []
--- src/create_env.py
+++
@@ -1,9 +0,0 @@
-import gymnasium as gym
-
-from src.modified_rewards import MountainCarContinuousRewardWrapper
-
-def create_env_continuous(env_id: str, seed: int = 0) -> gym.Env:
-    env = gym.make(env_id)
-    env = MountainCarContinuousRewardWrapper(env)
-
-    return env
